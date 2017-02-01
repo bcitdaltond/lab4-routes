d2526611838ca3993812fe9249891c5f945c970c
Wayne <wsyccc@hotmail.com>
2017-02-01 12:52:34 -0800

    fixed bingo

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['^[a-zA-Z]{4}/bingo'] = 'bingo';
 $route['sleep'] = 'first/zzz';
 $route['lock/em/up'] = 'welcome/shucks';
 $route['default_controller'] = 'welcome';
