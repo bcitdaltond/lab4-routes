52101bb3f0f1de4e98b2e87d744de9c606968367
RyanSirot <torisnayr@gmail.com>
2017-02-01 11:36:01 -0800

    #1 closed first_link

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -1,12 +1,30 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: To Olympus
- * Date: 2017-02-01
- * Time: 10:34 AM
- */
-class First
-{
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class First extends Application{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Homepage for our app
+     */
+    public function index()
+    {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'justone';
+
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get(1);
+
+        $this->data['mug'] = $source['mug'];
+        $this->data['who'] = $source['who'];
+        $this->data['what'] = $source['what'];
+
+        $this->render();
+    }
 
 }
